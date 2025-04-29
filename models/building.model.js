6009b569744dcc2d84f25a1675d8db99df507f39
Stoyan <s.imandiev@is-bg.net>
2025-04-29 13:49:07 +0300

    building edit and delete

--- models/building.model.js
+++ models/building.model.js
@@ -14,13 +14,37 @@ class Building {
   }
 
   static async getAll() {
-    const query = 'SELECT * FROM buildings';
+    const query = 'SELECT * FROM buildings WHERE is_deleted = false';
     const result = await pool.query(query);
     return result.rows;
   }
 
   static async getById(id) {
-    const query = 'SELECT * FROM buildings WHERE id = $1';
+    const query = 'SELECT * FROM buildings WHERE id = $1 AND is_deleted = false';
+    const result = await pool.query(query, [id]);
+    return result.rows[0];
+  }
+
+  static async update(id, buildingData) {
+    const { name, address, total_floors, description } = buildingData;
+    const query = `
+      UPDATE buildings 
+      SET name = $1, address = $2, total_floors = $3, description = $4
+      WHERE id = $5 AND is_deleted = false
+      RETURNING *
+    `;
+    const values = [name, address, total_floors, description || null, id];
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  }
+
+  static async delete(id) {
+    const query = `
+      UPDATE buildings 
+      SET is_deleted = true 
+      WHERE id = $1 AND is_deleted = false
+      RETURNING id
+    `;
     const result = await pool.query(query, [id]);
     return result.rows[0];
   }
